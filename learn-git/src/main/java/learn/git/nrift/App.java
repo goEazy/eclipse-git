e727230f0382732745452087f4271e3daba807c3
harsha <harsha@harsha.nrifintech.com>
2018-09-06 14:49:59 +0530

    First Commit

---
+++ learn-git/src/main/java/learn/git/nrift/App.java
@@ -0,0 +1,13 @@
+package learn.git.nrift;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
